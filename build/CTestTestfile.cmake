acd5cc9065348cc40e0197c59287181ede62b0f9
vasilevmi <misha.vasilev.10@bk.ru>
2024-04-15 17:03:16 +0700

    h

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/learning/уппрпо/project-git
-# Build directory: C:/learning/уппрпо/project-git/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("libraries/googletest")
-subdirs("tests")
